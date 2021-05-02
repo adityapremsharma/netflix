d45d81c94825286bedb5965d864b8e48ed37def1
unknown <adityapremsharma@gmail.com>
2021-05-03 00:54:31 +0530

    fix styles

--- src/screens/Message.js
+++ src/screens/Message.js
@@ -7,7 +7,12 @@ export default function Message() {
       <p>
         This demo is a UI based project and the API used in it doesn't contain
         any content.
+        <br />
+        <br />
+        Developed with ❤️ by Aditya Prem Sharma using React.JS for practice
+        purposes.
       </p>
+
       <Link to="/">
         <button className="btn-secondary">Go back</button>
       </Link>
