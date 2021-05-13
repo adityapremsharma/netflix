cce4d32bcdbb02136cd69a7f36a1d4e3fddb1417
unknown <adityapremsharma@gmail.com>
2021-05-14 01:34:33 +0530

    add service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "v1";
+
+const urlsToCache = ["index.html"];
+
+const self = this;
+
+// Install the service worker
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Listen to requests
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(() => {
+      return fetch(e.request).catch(() => caches.match("index.html"));
+    })
+  );
+});
+
+// Activate the service worker
+self.addEventListener("activate", (e) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
