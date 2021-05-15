67b19a7cee964ee9e1ad36c8a25d7b3237ea1718
unknown <adityapremsharma@gmail.com>
2021-05-15 18:20:14 +0530

    add offline alert

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Home from "./screens/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Message from "./screens/Message";
+import NoInternet from "./components/common/NoInternet";
 
 function App() {
   return (
     <BrowserRouter>
+      <NoInternet />
       <Switch>
         <Route exact path="/">
           <Home />
---
+++ src/components/common/NoInternet.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+export default function NoInternet() {
+  const [onlineStatus, setOnlineStatus] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("online", () => setOnlineStatus(true));
+    window.addEventListener("offline", () => setOnlineStatus(false));
+
+    return () => {
+      window.removeEventListener("online", null);
+      window.removeEventListener("offline", null);
+    };
+  }, []);
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    !onlineStatus && (
+      <div className="no-internet">
+        <h3>
+          Please check your internet connection.
+          <span onClick={reloadPage}>Reload</span>
+        </h3>
+      </div>
+    )
+  );
+}
